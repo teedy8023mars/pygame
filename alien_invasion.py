c753cda4f5b7693a4381d37ceb56b63410d748e9
Teddy_ziyu <459706193@qq.com>
2020-12-25 10:57:20 +0800

    12-25
    added a group of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,26 +1,63 @@
-import sys
+import sys, os, random
+from pygame import *
 import pygame
 from pygame.constants import K_RIGHT, QUIT, RESIZABLE
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
+from pygame.locals import *
+
+N = 200
+SCREEN_W, SCREEN_H = (640, 480)
 
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        #windows_mode
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         #fullscreen_mode
         # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         # self.settings.screen_width = self.screen.get_rect().width
         # self.settings.screen_height = self.screen.get_rect().height
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background = self.background.convert()
+        self.stars = [[
+            random.randint(0, SCREEN_W),
+            random.randint(0, SCREEN_H)
+        ] for x in range(N)]
+        self.clock = pygame.time.Clock()
+
         pygame.display.set_caption("FuCk those Aliens")
         self.ship = Ship(self)
         #store the bullet in group
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
+
+    #一只船队
+    def _create_fleet(self):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width
+        number_aliens_x = available_space_x // (2 * alien_width)
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (5 * alien_height) -
+                             ship_height)
+        number_rows = available_space_y // (3 * alien_height)
+        for row in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row)
+
+    def _create_alien(self, alien_number, row):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien_height + 2 * alien_height * row
+        self.aliens.add(alien)
 
     def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -62,18 +99,29 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         pygame.display.flip()
 
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
+            print(len(self.bullets))
+
             self._update_screen()
+            pygame.display.flip()
 
 
 if __name__ == '__main__':
--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship:
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
-        self.image = pygame.image.load('ship.png')
+        self.image = pygame.image.load('./pics/ship.png')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
